64e7d12d8a18d9bd02bb27654ead492d3f7c4ff5
exromany <exromany@lido.fi>
2026-08-28 22:48:15 +0300

    test: cover cache preservation, admin control and ossification

--- test/Proxy.t.sol
+++ test/Proxy.t.sol
@@ -6,6 +6,7 @@ import {SMDiscovery} from "../src/SMDiscovery.sol";
 import {OssifiableProxy} from "../src/lib/proxy/OssifiableProxy.sol";
 import {StakingRouterMock} from "./mocks/StakingRouterMock.sol";
 import {StakingModuleMock} from "./mocks/StakingModuleMock.sol";
+import {SMDiscoveryV2Mock} from "./mocks/SMDiscoveryV2Mock.sol";
 
 contract ProxyTest is Test {
     uint256 internal constant MODULE_ID = 3;
@@ -47,5 +48,57 @@ contract ProxyTest is Test {
             .moduleCache(MODULE_ID);
         assertEq(moduleAddress, address(module));
         assertEq(accountingAddress, ACCOUNTING);
+    }
+
+    function test_upgrade_preservesModuleCache() external {
+        discovery.updateModuleCache(MODULE_ID);
+        (address moduleBefore, address accountingBefore) = discovery
+            .moduleCache(MODULE_ID);
+
+        SMDiscoveryV2Mock v2 = new SMDiscoveryV2Mock(address(router));
+        vm.prank(ADMIN);
+        proxy.proxy__upgradeTo(address(v2));
+
+        (address moduleAfter, address accountingAfter) = discovery.moduleCache(
+            MODULE_ID
+        );
+        assertEq(moduleAfter, moduleBefore);
+        assertEq(accountingAfter, accountingBefore);
+        assertEq(proxy.proxy__getImplementation(), address(v2));
+        assertEq(SMDiscoveryV2Mock(address(proxy)).VERSION(), 2);
+    }
+
+    function test_upgrade_revertsForNonAdmin() external {
+        SMDiscoveryV2Mock v2 = new SMDiscoveryV2Mock(address(router));
+        vm.prank(address(0xBEEF));
+        vm.expectRevert(OssifiableProxy.NotAdmin.selector);
+        proxy.proxy__upgradeTo(address(v2));
+    }
+
+    function test_ossify_blocksFurtherUpgrades() external {
+        vm.prank(ADMIN);
+        proxy.proxy__ossify();
+        assertTrue(proxy.proxy__getIsOssified());
+        assertEq(proxy.proxy__getAdmin(), address(0));
+
+        SMDiscoveryV2Mock v2 = new SMDiscoveryV2Mock(address(router));
+        vm.prank(ADMIN);
+        vm.expectRevert(OssifiableProxy.ProxyIsOssified.selector);
+        proxy.proxy__upgradeTo(address(v2));
+    }
+
+    function test_ossify_revertsForNonAdmin() external {
+        vm.prank(address(0xBEEF));
+        vm.expectRevert(OssifiableProxy.NotAdmin.selector);
+        proxy.proxy__ossify();
+    }
+
+    function test_ossifiedProxy_stillServesReads() external {
+        discovery.updateModuleCache(MODULE_ID);
+        vm.prank(ADMIN);
+        proxy.proxy__ossify();
+        assertEq(address(discovery.STAKING_ROUTER()), address(router));
+        (address moduleAddress, ) = discovery.moduleCache(MODULE_ID);
+        assertEq(moduleAddress, address(module));
     }
 }
